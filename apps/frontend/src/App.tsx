576efff05fc79fde353e52195f1bdf09beb6b109
Gurnav224 <chaudharyg856@gmail.com>
2025-08-06 19:41:15 +0530

    Initial turborepo setup

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+     <h1 className='flex justify-center items-center h-[100vh] text-4xl'>Welcome to Social Media stream</h1>
+    </div>
+  )
+}
+
+export default App
