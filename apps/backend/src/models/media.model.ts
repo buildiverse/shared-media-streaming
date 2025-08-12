4e62283bc6af34db42d80e6d954a73dcf0e9203b
Gurnav224 <chaudharyg856@gmail.com>
2025-08-12 21:11:40 +0530

    design database model

---
+++ apps/backend/src/models/media.model.ts
@@ -0,0 +1,14 @@
+// backend/src/models/Media.js
+import mongoose from 'mongoose';
+
+const mediaSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  mimeType: { type: String },
+  size: { type: Number },
+  duration: { type: Number },
+  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('Media', mediaSchema);
