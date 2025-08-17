c197f3e02d5e43f09b953d067f4c3af7ae3e1bc5
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-08-17 17:15:17 +0000

    Run prettier format

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwind from "@tailwindcss/vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwind from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
   server: {
-    host: true,          
-    port: 5173,          
+    host: true,
+    port: 5173,
     watch: {
-      usePolling: true   
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+});
