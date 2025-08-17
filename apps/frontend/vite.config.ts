7dcf9d761358996a5dc11a57f28167e18f83717e
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-08-17 17:14:12 +0000

    Fix vite run

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,4 +5,11 @@ import tailwind from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  server: {
+    host: true,          
+    port: 5173,          
+    watch: {
+      usePolling: true   
+    }
+  }
 })
